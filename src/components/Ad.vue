e6fbcd983545ffb0526fbc0281f6340127bf13a4
kangkaimin <kkm_cn@163.com>
2017-11-12 17:55:27 +0800

    微信

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -120,7 +120,7 @@
           alert(ret.jsapi_ticket);
           alert(ret.nonceStr);
           alert(ret.timestamp);
-          alert(location.href);
+          alert(ret.signature);
 
           var sha1 = crypto.createHash('sha1');
           sha1.update(string1);
@@ -128,7 +128,7 @@
 
           wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: 'wxb4d337ae696167c6', // 必填，公众号的唯一标识
+            appId: appId, // 必填，公众号的唯一标识
             timestamp: ret.timestamp,// 必填，生成签名的时间戳
             nonceStr: ret.nonceStr, // 必填，生成签名的随机串
             signature: ret.signature,// 必填，签名，见附录1
