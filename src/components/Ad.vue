86786867a32fc00a1a4f677596516cb195a4105a
kangkaimin <kkm_cn@163.com>
2017-11-12 17:57:20 +0800

    微信

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -116,15 +116,16 @@
             "&timestamp=" + ret.timestamp +
             "&url=" + ret.url;
 
+
+          var sha1 = crypto.createHash('sha1');
+          sha1.update(string1);
+          ret.signature = sha1.digest('hex');
+
           alert("===================================================");
           alert(ret.jsapi_ticket);
           alert(ret.nonceStr);
           alert(ret.timestamp);
           alert(ret.signature);
-
-          var sha1 = crypto.createHash('sha1');
-          sha1.update(string1);
-          ret.signature = sha1.digest('hex');
 
           wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
