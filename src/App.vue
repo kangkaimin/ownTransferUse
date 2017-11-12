0efe4326899b8ad95f8f76e78b330f467faf0d95
kangkaimin <kkm_cn@163.com>
2017-11-12 12:18:53 +0800

    微信

--- src/App.vue
+++ src/App.vue
@@ -81,10 +81,10 @@
         url: url
       };
 
-      var string = raw(ret);
-      jsSHA = require('jssha');
-      shaObj = new jsSHA(string, 'TEXT');
-      ret.signature = shaObj.getHash('SHA-1', 'HEX');
+//      var string = raw(ret);
+//      jsSHA = require('jssha');
+//      shaObj = new jsSHA(string, 'TEXT');
+//      ret.signature = shaObj.getHash('SHA-1', 'HEX');
 
 //      wx.config({
 //        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
